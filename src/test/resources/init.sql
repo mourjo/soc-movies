b69b1e3d7892f536a4f400f38ee804e366172a37
Mourjo Sen <hello@mourjo.me>
2024-08-25 12:22:08 +0200

    Add test database

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -37,3 +37,33 @@ CREATE TABLE ratings (
    rating NUMERIC(5,2),
    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+
+\c soc_movies_test_db;
+
+CREATE TABLE users (
+   id serial PRIMARY KEY,
+   username TEXT UNIQUE,
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+
+CREATE TABLE movies (
+   id serial PRIMARY KEY,
+   name TEXT UNIQUE,
+   slug TEXT UNIQUE,
+   description TEXT,
+   tags TEXT,
+   released_year NUMERIC(4),
+   lang TEXT,
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+
+CREATE TABLE ratings (
+   id serial PRIMARY KEY,
+   user_id integer REFERENCES users(id) NOT NULL,
+   movie_id integer REFERENCES movies(id) NOT NULL,
+   rating NUMERIC(5,2),
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
